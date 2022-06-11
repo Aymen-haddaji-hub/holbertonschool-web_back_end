97ee4da97408d507a098b0f532961748dac52624
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-06-11 14:12:03 +0100

    advanced typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -9,6 +9,7 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     a type-annotated function element_length that takes an
     Iterable[Sequence] as argument and returns a List[Tuple[Sequence, int]].
     The first element of each tuple is the Sequence.
-    The second element is the length of the Sequence and should be annotated as an int.
+    The second element is the length of the Sequence
+    and should be annotated as an int.
     """
     return [(i, len(i)) for i in lst]
